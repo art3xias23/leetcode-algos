6b301e11fcdafac52df691191dc33aac7417da46
Tino <Konstantin.v.milchev@gmail.com>
2024-09-13 23:13:14 +0100

    added slow solution to Minimum_WIndow_Substring

---
+++ Algos.Test/Minimum_Window_Substring_Tests.cs
@@ -0,0 +1,94 @@
+﻿using Algos.Array;
+using NUnit.Framework;
+
+namespace Algos.Tests
+{
+    [TestFixture]
+    public class Minimum_Window_Substring_Tests
+    {
+        private Minimum_Window_Substring _logic;
+        [SetUp]
+        public void SetUp()
+        {
+            _logic = new Minimum_Window_Substring();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            var input = "ADOBECODEBANC";
+            var t = "ABC";
+            var expected = "BANC";
+
+            var actual = _logic.MinWindow(input, t);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+
+             var input = "a";
+            var t = "a";
+            var expected = "a";
+
+            var actual = _logic.MinWindow(input, t);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test3()
+        {
+
+            var input = "a";
+            var t = "aa";
+            var expected = "";
+
+            var actual = _logic.MinWindow(input, t);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+         [Test]
+        public void Test4()
+        {
+
+            var input = "a";
+            var t = "b";
+            var expected = "";
+
+            var actual = _logic.MinWindow(input, t);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+         [Test]
+        public void Test5()
+        {
+
+            var input = "a";
+            var t = "AB";
+            var expected = "";
+
+            var actual = _logic.MinWindow(input, t);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+          [Test]
+        public void Test6()
+        {
+
+            var input = "aa";
+            var t = "aa";
+            var expected = "aa";
+
+            var actual = _logic.MinWindow(input, t);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
