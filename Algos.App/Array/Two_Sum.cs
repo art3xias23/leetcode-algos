be9d6cd14da752014cc6b1b4a3bdc4160ec12406
Tino <Konstantin.v.milchev@gmail.com>
2024-09-14 23:41:19 +0100

    added solution for Two SUm

---
+++ Algos.App/Array/Two_Sum.cs
@@ -0,0 +1,20 @@
+﻿namespace Algos.Array
+{
+    [Id(1)]
+    public class Two_Sum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var l = 0; l < nums.Length; l++)
+            {
+                for (var r = l + 1; r < nums.Length; r++)
+                {
+
+                    if (nums[l] + nums[r] == target)
+                        return [l, r];
+                }
+            }
+            return [];
+        }
+    }
+}
---
+++ Algos.Test/Two_Sum_Tests.cs
@@ -0,0 +1,87 @@
+﻿namespace Algos.Tests
+{
+    using Algos.Array;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Two_Sum_Tests
+    {
+        private Two_Sum _solution;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new Two_Sum();
+        }
+
+        [Test]
+        public void TwoSum_WhenNumsAre2_7_11_15_AndTargetIs9_Returns0_1()
+        {
+            // Arrange
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(new int[] { 0, 1 }, result);
+        }
+
+        [Test]
+        public void TwoSum_WhenNumsAre3_2_4_AndTargetIs6_Returns1_2()
+        {
+            // Arrange
+            int[] nums = { 3, 2, 4 };
+            int target = 6;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(new int[] { 1, 2 }, result);
+        }
+
+        [Test]
+        public void TwoSum_WhenNumsAre3_3_AndTargetIs6_Returns0_1()
+        {
+            // Arrange
+            int[] nums = { 3, 3 };
+            int target = 6;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(new int[] { 0, 1 }, result);
+        }
+
+         [Test]
+        public void TwoSum_WhenTargetIs0_Returns0()
+        {
+            // Arrange
+            int[] nums = { 0,4,3,0};
+            int target = 0;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(new int[] { 0, 3 }, result);
+        }
+        
+         [Test]
+        public void TwoSum_5()
+        {
+            // Arrange
+            int[] nums = {-3,4,3,90};
+            int target = 0;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(new int[]{0, 2} , result);
+        }
+    }
+}
