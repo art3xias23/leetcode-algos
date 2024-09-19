a941bd3d558f7d6ca8e7144808b8aef7f2a2247c
Tino <Konstantin.v.milchev@gmail.com>
2024-09-19 21:26:02 +0100

    added solution for 206

---
+++ Algos.App/0206_Reverse_Linked_List.cs
@@ -0,0 +1,31 @@
+﻿using Algos.Data;
+
+namespace Algos
+{
+    public class _206_Reverse_Linked_List
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            if (head.next == null)
+            {
+                return head;
+            }
+
+            ListNode prev = null;
+            while (head != null)
+            {
+                var next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+
+            return prev;
+        }
+    }
+}
