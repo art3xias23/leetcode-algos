1b196737166285ab3df08c8f6ff3ce38fb784f5b
Tino <Konstantin.v.milchev@gmail.com>
2024-09-24 22:54:14 +0100

    added brutje force for 739

---
+++ Algos.App/0739_Daily_Temperatures.cs
@@ -0,0 +1,13 @@
+﻿namespace Algos
+{
+    public class _739_Daily_Temperatures
+    {
+        public static int[] DailyTemperatures(int[] temperatures)
+        {
+            var result = new int[temperatures.Length];
+            var stack = new Stack<(int, int)>();
+            for(int i = 0;)
+                
+        }
+    }
+}
---
+++ Algos.Test/0739_Daily_Temperatures_Tests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+
+namespace Algos.Tests
+{
+    [TestFixture]
+    internal class _0739_Daily_Temperatures_Tests
+    {
+        [Test]
+        public void TestExample1()
+        {
+            // Arrange
+            var temperatures = new int[] { 73, 74, 75, 71, 69, 72, 76, 73 };
+            var expected = new int[] { 1, 1, 4, 2, 1, 1, 0, 0 };
+
+            // Act
+            var result = _739_Daily_Temperatures.DailyTemperatures(temperatures);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestExample2()
+        {
+            // Arrange
+            var temperatures = new int[] { 30, 40, 50, 60 };
+            var expected = new int[] { 1, 1, 1, 0 };
+
+            // Act
+            var result = _739_Daily_Temperatures.DailyTemperatures(temperatures);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestExample3()
+        {
+            // Arrange
+            var temperatures = new int[] { 30, 60, 90 };
+            var expected = new int[] { 1, 1, 0 };
+
+            // Act
+            var result = _739_Daily_Temperatures.DailyTemperatures(temperatures);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestDecreasingTemperatures()
+        {
+            // Arrange
+            var temperatures = new int[] { 100, 90, 80, 70 };
+            var expected = new int[] { 0, 0, 0, 0 }; // No higher temperatures ahead
+
+            // Act
+            var result = _739_Daily_Temperatures.DailyTemperatures(temperatures);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestAllSameTemperatures()
+        {
+            // Arrange
+            var temperatures = new int[] { 50, 50, 50, 50 };
+            var expected = new int[] { 0, 0, 0, 0 }; // No higher temperatures ahead
+
+            // Act
+            var result = _739_Daily_Temperatures.DailyTemperatures(temperatures);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestSingleTemperature()
+        {
+            // Arrange
+            var temperatures = new int[] { 70 };
+            var expected = new int[] { 0 }; // Only one temperature, no future day
+
+            // Act
+            var result = _739_Daily_Temperatures.DailyTemperatures(temperatures);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
