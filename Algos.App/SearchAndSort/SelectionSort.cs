20819b3b2c20a2c163f547dea211e3342820c21e
Tino <Konstantin.v.milchev@gmail.com>
2024-09-28 22:52:08 +0100

    added selection sort

---
+++ Algos.App/SearchAndSort/SelectionSort.cs
@@ -0,0 +1,29 @@
+﻿namespace Algos.SearchAndSort
+{
+    public class SelectionSort
+    {
+        public void Sort(int[] initial)
+        {
+            for (int i = 0; i < initial.Length; i++)
+            {
+                var minIndex = i;
+
+                for (int j = i + 1; j < initial.Length; j++)
+                {
+                    if (initial[minIndex] > initial[j])
+                        minIndex = j;
+                }
+
+                if (minIndex != i)
+                    Swap(initial, i, minIndex);
+            }
+        }
+
+        private void Swap(int[] init, int left, int right)
+        {
+            var temp = init[left];
+            init[left] = init[right];
+            init[right] = temp;
+        }
+    }
+}
