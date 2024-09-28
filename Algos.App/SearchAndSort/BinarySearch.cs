6ce99f846384797a306621bcc8b44eaaa50d5d01
Tino <Konstantin.v.milchev@gmail.com>
2024-09-28 13:10:28 +0100

    added binary search

---
+++ Algos.App/SearchAndSort/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿namespace Algos.SearchAndSort
+{
+    public class BinarySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length == 0) return -1;
+
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while(left <= right)
+            {
+               var midIndex = left + ((right-left)/2);
+                Console.WriteLine(midIndex);
+                if(nums[midIndex] == target) return midIndex;
+
+                if(nums[midIndex] > target)
+                    right = midIndex-1;
+                else
+                    left = midIndex+1;
+            }
+
+            return -1;
+
+        }
+    }
+}
---
+++ Algos.Test/SearchAndSort/Binary_Search_Tests.cs
@@ -0,0 +1,117 @@
+﻿using Algos.SearchAndSort;
+using NUnit.Framework;
+
+namespace Algos.Tests.SearchAndSort
+{
+    [TestFixture]
+    public class Binary_Search_Tests
+    {
+
+        private BinarySearch binarySearch;
+
+        [SetUp]
+        public void SetUp()
+        {
+            binarySearch = new BinarySearch();
+        }
+
+        [Test]
+        public void Test_TargetInMiddle()
+        {
+            int[] nums = { 1, 3, 5, 7, 9 };
+            int target = 5;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Test_TargetIsFirstElement()
+        {
+            int[] nums = { 1, 3, 5, 7, 9 };
+            int target = 1;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Test_TargetIsLastElement()
+        {
+            int[] nums = { 1, 3, 5, 7, 9 };
+            int target = 9;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Test_TargetNotInArray()
+        {
+            int[] nums = { 1, 3, 5, 7, 9 };
+            int target = 6;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Test_EmptyArray()
+        {
+            int[] nums = { };
+            int target = 3;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Test_SingleElementArray_Found()
+        {
+            int[] nums = { 5 };
+            int target = 5;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Test_SingleElementArray_NotFound()
+        {
+            int[] nums = { 5 };
+            int target = 3;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Test_ArrayWithDuplicates_Found()
+        {
+            int[] nums = { 1, 2, 4, 4, 4, 7, 9 };
+            int target = 4;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.IsTrue(result == 2 || result == 3 || result == 4);
+        }
+
+        [Test]
+        public void Test_ArrayWithNegativeNumbers()
+        {
+            int[] nums = { -10, -5, 0, 5, 10 };
+            int target = -5;
+
+            int result = binarySearch.Search(nums, target);
+
+            Assert.AreEqual(1, Math.Abs(result));
+        }
+    }
+}
