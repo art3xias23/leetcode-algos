ad22d28617f3ece1b2bf3f01d67f89b6b511ceca
Tino <Konstantin.v.milchev@gmail.com>
2024-09-30 22:30:22 +0100

    another merge sort

--- Algos.App/SearchAndSort/MergeSort.cs
+++ Algos.App/SearchAndSort/MergeSort.cs
@@ -9,7 +9,6 @@ namespace Algos.SearchAndSort
         {
             System.Diagnostics.Debug.WriteLine("Original Array: " + string.Join(", ", arr));
 
-            // Call MergeSort on the array
             MergeSrt(arr, 0, arr.Length - 1);
 
             System.Diagnostics.Debug.WriteLine("Sorted Array: " + string.Join(", ", arr));
@@ -18,70 +17,76 @@ namespace Algos.SearchAndSort
         }
         //we initially pass arr.Length - 1 as right;
 
-        private void MergeSrt(int[] arr, int left, int right, int counter = 0)
-        {
-            if (left < right)
-            {
-                var mid = (right + left) / 2;
+        //we initially pass arr.Length - 1 as right;
 
-                counter++;
-                System.Diagnostics.Debug.WriteLine($"Left{counter}: {string.Join(" ", arr[left..(mid+1)])}");
-                MergeSrt(arr, left, mid, counter);
-                System.Diagnostics.Debug.WriteLine($"Right{counter}: {string.Join(" ", arr[(mid + 1)..(right + 1)])}");
-                MergeSrt(arr, mid + 1, right, counter);
+        private void MergeSrt(int[] arr, int left, int right)
+        {
+            if (right > left)
+            {
+                var mid = (left + right) / 2;
+                MergeSrt(arr, left, mid);
+                MergeSrt(arr, mid + 1, right);
 
                 Merge(arr, left, right, mid);
-                System.Diagnostics.Debug.WriteLine($"{string.Join(" ", arr)}");
+                System.Diagnostics.Debug.WriteLine($"Arr:{string.Join(" ", arr)}");
             }
         }
 
         private void Merge(int[] arr, int left, int right, int mid)
         {
-
-            //Set size
+            //set size
             var leftSize = mid - left + 1;
             var rightSize = right - mid;
 
+
+            //set arrays
             var leftArr = new int[leftSize];
             var rightArr = new int[rightSize];
 
+            //populate arrays
             for (int i = 0; i < leftSize; i++)
             {
-                leftArr[i] = arr[i + left];
+                leftArr[i] = arr[left + i];
             }
-
             for (int i = 0; i < rightSize; i++)
             {
-                rightArr[i] = arr[mid + 1 + i];
+                rightArr[i] = arr[mid + i + 1];
             }
 
+            //set variables
+            var iLeft = 0; var iRight = 0; var iMerged = left;
 
-            int iLeft = 0; int iRight = 0; int iMerged = left;
-
+            //loop
             while (iLeft < leftSize && iRight < rightSize)
             {
                 var leftItem = leftArr[iLeft];
                 var rightItem = rightArr[iRight];
-                if (leftItem > rightItem)
+
+                if (leftItem < rightItem)
                 {
-                    arr[iMerged++] = rightArr[iRight++];
+                    arr[iMerged++] = leftArr[iLeft++];
                 }
                 else
                 {
-
-                    arr[iMerged++] = leftArr[iLeft++];
+                    arr[iMerged++] = rightArr[iRight++];
                 }
+
             }
 
             while (iLeft < leftSize)
             {
                 arr[iMerged++] = leftArr[iLeft++];
             }
-
             while (iRight < rightSize)
             {
                 arr[iMerged++] = rightArr[iRight++];
             }
+            //loop leftovers
         }
+
+
+
+
+
     }
 }
